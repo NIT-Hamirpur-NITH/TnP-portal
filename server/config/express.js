32989abdf1832e2c7bc68e6dad9cf36f266af1fd
prikshittekta <prikshittekta@gmail.com>
2016-03-17 23:49:44 +0530

    added passport

--- server/config/express.js
+++ server/config/express.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var bodyParser = require('body-parser');
+var passport = require('passport');
 var path = require('path');
 var logger = require('morgan');
 
@@ -11,8 +13,13 @@ module.exports = function(app, config){
   app.set('view engine', 'html');
   //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
   app.use(logger('dev'));
+  app.use(cookieParser());
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(cookieParser());
+  app.use(session({ secret: 'youvians',
+					          resave: true,
+  					        saveUninitialized: true }));
+  app.use(passport.initialize());
+  app.use(passport.session());
   app.use(express.static(path.join(config.rootPath, '/public')));
 }
