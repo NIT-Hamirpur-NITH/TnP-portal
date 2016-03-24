8daeb39c63a8e9a1a77f7755c18a0b55050e4e35
prikshittekta <prikshittekta@gmail.com>
2016-03-24 21:50:51 +0530

    added font lato

---
+++ public/fonts/lato/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+        Error-404
+    </title>
+</head>
+<body>
+    <p>
+        <i>
+            <b>Error 404:</b>The page you requested is not available.
+        </i>
+    </p>
+</body>
+</html>
