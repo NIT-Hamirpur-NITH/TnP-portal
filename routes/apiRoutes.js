62e300f65979f9b7bb923f2044955128ed03c5dd
Parth Gupta <parthgupta.0912@gmail.com>
2016-04-02 01:33:25 -0700

    added placedIn route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,9 +2,11 @@ var express = require('express');
 var router = express.Router();
 var auth = require('../api/auth/auth');
 var appliedFor = require('../api/appliedFor');
+var placedIn = require('../api/placedIn');
 
 router.post('/login', auth.alreadyLoggedIn, auth.loginAuthenticate);
 router.post('/logout', auth.logout);
 router.get('/appliedfor', auth.isLoggedIn, appliedFor.appliedFor);
+router.get('/placedIn', auth.isLoggedIn, placedIn.placedIn);
 
 module.exports = router;
