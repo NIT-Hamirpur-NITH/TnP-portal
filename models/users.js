4e361368527c549d94f30fc918b8a5a7cf156670
prikshittekta <prikshittekta@gmail.com>
2016-04-07 03:37:28 +0530

    updated users model

--- models/users.js
+++ models/users.js
@@ -2,16 +2,19 @@ var mongoose = require('mongoose');
 var schema = mongoose.Schema;
 
 var userSchema = new schema ({
+	sno:{type: Number, required:true, unique:true},
 	username: {type: Number, required: true, unique: true},
 	password: {type: String, required: true},
-	name: {type: String,required:true},
-	branch: {type: String,required:true},
+	name: {type: String, required: true},
+	email: {type: String, required: true},
+	branch: {type: String, required:true},
 	appliedFor: [String],
 	placedIn: [String],
 	roles: [String],
 	tenth: {type: Number, required: true},
 	twelfth: {type: Number, required: true},
-	btech: {type: Number, required: true}
+	btech: {type: Number, required: true},
+	invite: {type: Boolean, default: 0}
 });
 
 var userModel = mongoose.model('users', userSchema);
